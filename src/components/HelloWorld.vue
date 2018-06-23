af55d09da4aa0018878dd4666debcf8c88b78a49
薛崇伟 <360240422@qq.com>
2018-06-23 08:20:28 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,170 @@
 <template>
-  <div class="hello">
+  <div id="drag">
     <h2>Vue Drag</h2>
-    
+    <!-- 左侧预览区域 start-->
+    <div id="drag-left">
+      <draggable class="drag-left-wrap" element="div" v-model="list2" :options="leftOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false">
+        <img src="@/assets/guide.jpg" class="guide-img" v-if="list2.length<=0">
+        <template v-for="item in list2">
+          <div>{{item.name}}</div>
+        </template>
+      </draggable>
+    </div>
+    <!-- 左侧预览区域 end-->
+
+    <!-- 右侧浮窗 start-->
+    <draggable id="drag-right" element="div" v-model="list" :options="rightOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false"> 
+      <transition-group type="transition" :name="'flip-list'" tag="ul">
+        <li v-for="item in list" :key="item.order">
+          <i class="iconfont icon-danxuan" v-if="item.type=='single'"></i>
+          <i class="iconfont icon-duoxuan" v-else-if="item.type=='multiple'"></i>
+          <span>{{item.name}}</span>
+        </li>
+      </transition-group>
+    </draggable>
+    <!-- 右侧浮窗 end-->
+
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
   name: 'Drag',
+  components: {
+    draggable,
+  },
   data () {
     return {
-     
+      editable:true,  //拖动状态
+      isDragging: false,  
+      delayedDragging:false,
+      // 右侧浮窗
+      list:[
+        {
+          "name": "单选",
+          "type": "single",
+          "order": 1,
+          "fixed": false
+        },
+        {
+          "name": "多选",
+          "type": "multiple",
+          "order": 2,
+          "fixed": false
+        },
+      ],
+      // 左侧题目显示
+      list2:[]
+    }
+  },
+  computed:{
+    // 右侧浮窗拖动配置
+    rightOptions () {
+      return  {
+        animation: 0,
+        group: {name:'right',pull:'clone',put:false},
+        disabled: !this.editable,
+        ghostClass: 'ghost'
+      };
+    },
+    leftOptions () {
+      return  {
+        animation: 0,
+        group: 'right',
+        disabled: !this.editable,
+        ghostClass: 'ghost'
+      };
+    },
+  },
+  methods:{
+    onMove ({relatedContext, draggedContext}) {
+      /*const relatedElement = relatedContext.element;
+      const draggedElement = draggedContext.element;
+      console.log(relatedElement);
+      console.log(draggedElement);
+      return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed*/
+    }
+  },
+  watch: {
+    isDragging (newValue) {
+      if (newValue){
+        this.delayedDragging= true
+        return
+      }
+      this.$nextTick( () =>{
+           this.delayedDragging =false
+      })
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="less">
+*{margin: 0;padding: 0;}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#drag{
+  
+  #drag-left{
+    position: relative;
+    width: 700px;
+    padding: 100px 0;
+    .guide-img{
+      margin-top: 120px;
+    }
+    .drag-left-wrap{
+      width: 100%;
+      li.sortable-chosen{
+        height: 200px;
+        border:2px dashed orange;
+        list-style: none;
+        & > *{display: none;}
+      }
+    }
+
+    /*test code*/
+    outline:1px dashed orange;
+    margin: 0 auto;
+  }
+  #drag-right{
+    position: fixed;
+    top: 229px;
+    right: 23px;
+    width: 125px;
+    background-color: #fff;
+    box-shadow:0 0 5px 3px rgba(162, 162, 162, 0.1);
+
+    > ul{
+      li{
+        width: 100%;
+        height: 50px;
+        line-height: 50px;
+        text-align: center;
+        cursor: pointer;
+        transition:background-color .3s;
+        &:hover{
+          background-color: #ECF5FF;
+        }
+      }
+    }
+  }
+  .flip-list-move {
+    transition: transform 0.5s;
+  }
+
+  /* .no-move {
+    transition: transform 0s;
+  } */
+
+  .ghost {
+    opacity: .5;
+    background: #C8EBFB;
+  }
+  
 }
 </style>
